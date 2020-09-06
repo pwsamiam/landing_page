560eaecb474e47147e21fa1f268b798daa8e0f3e
pwsamiam <pwsamiam@gmail.com>
2020-09-05 23:26:08 -0400

    Updates

---
+++ precache-manifest.0ff51bcf770f1f7d777341f164a812f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d49e27867140ccc0fda1239f52b0b8c",
+    "url": "/landing_page/index.html"
+  },
+  {
+    "revision": "ab70a317fb3c91426ace",
+    "url": "/landing_page/static/css/main.5c7eccfa.chunk.css"
+  },
+  {
+    "revision": "c28659ad857b7fc38b46",
+    "url": "/landing_page/static/js/2.c5143352.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing_page/static/js/2.c5143352.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab70a317fb3c91426ace",
+    "url": "/landing_page/static/js/main.14ac8ec7.chunk.js"
+  },
+  {
+    "revision": "9247ddfe539230cb23f3",
+    "url": "/landing_page/static/js/runtime-main.3a45aad2.js"
+  }
+]);
