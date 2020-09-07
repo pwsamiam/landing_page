72fd3b16f417fe00d2b06cd09f8b211ea861a812
pwsamiam <pwsamiam@gmail.com>
2020-09-06 20:05:48 -0400

    update

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ const FooterContainer = () => {
 				              <small>&copy; Jack's Creek Productions All Rights Reserved. <p>Design by: <a href="https://jacks-creek.com/">Jack's Creek Productions</a> | Banner image by: <a href="https://jacks-creek.com">jacks-creek.com</a></p></small>    
 	                 <small> <p>*Disclaimer: All opinions and commentary are solely that of www.jacks-creek.com</p></small>			                    
                   <small> <p>Contact: <a href="mailto:adminvps@jacks-creek.com"> adminvps@jacks-creek.com</a></p></small>
-                  <p><a href="https://twitter.com"><input type="image" src={require('./images/twitterFooter.webp')} id="twitter" title="Twitter" alt="Twitter" /></a></p>
+                  <p><a href="https://twitter.com/JacksCreekcom"><input type="image" src={require('./images/twitterFooter.webp')} id="twitter" title="Twitter" alt="Twitter" /></a></p>
               </div>   
 		        </div>
         </div>	
